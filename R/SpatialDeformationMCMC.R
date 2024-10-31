afdc91f9fc56786318942ea9635a69f2b987b5b9
Carla <carlaapolinario960@gmail.coml>
2024-10-31 19:51:42 -0300

    first commit

---
+++ R/SpatialDeformationMCMC.R
@@ -0,0 +1,216 @@
+#' Title
+#'
+#' @param response
+#' @param covariate
+#' @param sites
+#' @param prior
+#' @param interact
+#' @param burnin
+#' @param jump
+#' @param indi
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+SpatialDeformationMCMC <- function(response, FT, MatFFT, GT, sites, GAMA,
+  prior=list(m0=as.matrix(rep(0,nrow(FT))),
+  mvarphi=1/18,
+  C0=diag(1000,nrow(FT)),
+  S0=diag(0.01,nrow(response)),
+  n0=1,
+  n0est=n0+nrow(data),
+  asigmad=10002,
+  bsigmad=10001,
+  asigmala=0.01,
+  bsigmala=0.01,
+  aepsilon=rep(0.01,nrow(GAMA)),
+  axi=rep(0.01,nrow(GAMA)),
+  bepsilon=rep(0.01,nrow(GAMA)),
+  bxi=rep(0.01,nrow(GAMA)),
+  aaphi=rep(0.01,nrow(GAMA)),
+  bbphi=rep(0.01,nrow(GAMA)),
+  u1phi=rep(500,nrow(GAMA))), iteration, burnin, jump){
+
+  qq <- nrow(GAMA)
+  Phis=rep(0.51,qq)
+  sigmas=rep(0.51,qq)
+  kappas=rep(0.51,qq)
+  varphi=0.51
+  sigmadd=0.51
+  sigmal=0.41
+  lambdas=matrix(0.000,qq,ncol(response)*2)
+
+  #Valores computacionais
+  uu1=rep(3.678794e-07,nrow(lambdas))
+  lambdaant=matrix(0,qq,ncol(lambdas))
+  VV=matrix(NA,qq,ncol(response)^2)
+  RR=matrix(NA,qq,ncol(response)^2)
+
+  indi=NULL
+
+  for (i in 1:nrow(GAMA)) {
+    indi <- c(indi,rep(i,ncol(GAMA)))
+  }
+
+  uuu1=rep(50,nrow(GAMA))
+  uuu2=rep(50,nrow(GAMA))
+
+  S=complex(real=sites[,1],imaginary=sites[,2])
+  stemp=H(nrow(sites))%*%S ## Removendo translação
+  Sz=t(H(nrow(sites)))%*%stemp ## configuração com translação removida
+  Sz=cbind(as.matrix(Re(Sz)),as.matrix(Im(Sz)))
+  Z0=as.vector(Sz)
+
+  MZ=NULL
+  for(l in 1:qq){
+    MZ=rbind(MZ,t(as.matrix(as.vector(Sz))))
+  }
+  Zest=matrix(NA,nrow(MZ),ncol(MZ))
+
+  bd=-2*log(0.5)/(max(dist(Sz)))
+  Delta=gCorr(bd,Sz)
+
+  Thetas=matrix(0,nrow(response),ncol(GT))
+  Theta0=as.matrix(rep(0,ncol(Thetas)))
+  Psis=diag(0.5,ncol(Thetas))
+
+  MDef=NULL
+  MDefT=NULL
+  MDef1T=NULL
+  MTheta=NULL
+  MTheta0=NULL
+  Mlambda=NULL
+  MPsi=NULL
+  Msigmak=NULL
+  Mkappa=NULL
+  MkappaT=NULL
+  MkappaT1=NULL
+  MPhi=NULL
+  MPhiT=NULL
+  MPhiT1=NULL
+  Mvarphi=NULL
+
+  for(j in 1:iteration){
+
+    if(j<=burnin){
+
+      DDefT <- NULL
+      ttt <- NULL
+      resphiT <- NULL
+
+
+      for (s in 1:qq) {
+        DEF <- matrix(MZ[s, ], ncol(response), 2)
+        VV[s, ] <- as.vector(sigmas[s] * ((1 - kappas[s]) * gCorr(Phis[s], DEF) + kappas[s] * diag(1, ncol(response))))
+        RR[s, ] <- as.vector(((1 - kappas[s]) * gCorr(Phis[s], DEF) + kappas[s] * diag(1, ncol(response))))
+        Zest[s, ] <- MZ[s, ] - Z0
+
+        temp <- amostrard(as.matrix(MZ[s, ]), Sz, Delta, MatFFT, response, Thetas, GAMA[s, ], kappas[s], sigmas[s], Phis[s], as.matrix(lambdas[s, ]), sigmadd, Delta, uu1[s])
+        MZ[s, ] <- temp[[1]]
+        DDefT <- c(DDefT, temp[[2]])
+
+        if ((j %% 50) == 0) {
+          uuu1[s] <- sintonizar(0.60, uuu1[s], c(MkappaT[, s], temp[[2]]), j)
+          uuu2[s] <- uuu1[s]
+          uu1[s] <- sintonizarN(0.20, uu1[s], c(MDefT[, s], temp[[2]]), j)
+          u1phi[s] <- sintonizar(0.60, u1phi[s], c(MPhiT[, s], temp[[2]]), j)
+        } else {
+
+        }
+
+
+        bbsigmak <- bxi[s] / (1 - kappas[s]) + bepsilon[s] / kappas[s] + 0.5 * SSUMmed(MatFFT[GAMA[s, ], ], response[GAMA[s, ], ], Thetas[GAMA[s, ], ], matrix(RR[s, ], ncol(response), ncol(response)))
+        absigmak <- aepsilon[s] + axi + ncol(GAMA) * ncol(response) / 2
+        sigmas[s] <- 1 / rgamma(1, absigmak, bbsigmak)
+
+
+        temp <- tryCatch(amostrarkapa(MatFFT[GAMA[s, ], ], response[GAMA[s, ], ], Thetas[GAMA[s, ], ], kappas[s], sigmas[s], Phis[s], MZ[s, ], aepsilon[s], axi[s], bepsilon[s], bxi[s], uuu1[s], uuu2[s]), error = function(e) e)
+
+        if (is.list(temp) == T) {
+          temp <- amostrarkapa(MatFFT[GAMA[s, ], ], response[GAMA[s, ], ], Thetas[GAMA[s, ], ], kappas[s], sigmas[s], Phis[s], MZ[s, ], aepsilon[s], axi[s], bepsilon[s], bxi[s], uuu1[s], uuu2[s])
+          kappas[s] <- temp[[1]]
+          ttt <- c(ttt, temp[[2]])
+        } else {
+          ttt <- c(ttt, temp[[2]])
+        }
+
+        temp <- tryCatch(amostrarphi(MatFFT[GAMA[s, ], ], response[GAMA[s, ], ], Thetas[GAMA[s, ], ], kappas[s], sigmas[s], Phis[s], MZ[s, ], aaphi[s], bbphi[s], u1phi[s]), error = function(e) e)
+
+        if (is.list(temp) == T) {
+          temp <- amostrarphi(MatFFT[GAMA[s, ], ], response[GAMA[s, ], ], Thetas[GAMA[s, ], ], kappas[s], sigmas[s], Phis[s], MZ[s, ], aaphi[s], bbphi[s], u1phi[s])
+          Phis[s] <- temp[[1]]
+          resphiT <- c(resphiT, temp[[2]])
+        } else {
+          resphiT <- c(resphiT, 0)
+        }
+      }
+
+      temp <- FFBSmod(MatFFT, GT, VV, Psis, m0, C0, response, Thetas, indi)
+      Thetas <- temp
+
+      CC0 <- solve(solve(C0) + t(GT) %*% solve(Psis) %*% GT)
+      mm0 <- CC0 %*% (solve(C0) %*% m0 + t(GT) %*% solve(Psis) %*% as.matrix(Thetas[1, ]))
+
+      Theta0 <- as.matrix(MASS::mvrnorm(1, mm0, CC0))
+
+      MDefT <- rbind(MDefT, t(as.matrix(DDefT)))
+      MkappaT <- rbind(MkappaT, t(as.matrix(ttt)))
+      MPhiT <- rbind(MPhiT, t(as.matrix(resphiT)))
+
+
+      temp <- FFBSdef(diag(1, 2 * ncol(response)), varphi * diag(1, 2 * ncol(response)), sigmadd * diag(1, 2) %x% Delta, sigmal * diag(1, 2) %x% Delta, as.matrix(rep(0, ncol(response) * 2)), diag(0.1, 2 * ncol(response)), Zest, lambdas)
+      lambdas <- temp
+
+      Thetaant <- rbind(t(Theta0), Thetas[1:(nrow(Thetas) - 1), ])
+      S0est <- solve(S0 + SSUMPsi(GT, Thetas, Thetaant))
+      temp <- solve(array(as.vector(rWishart(1, n0est, S0est)), dim = c(ncol(GT), ncol(GT))))
+      Psis <- temp
+
+
+      lambdaant[2:qq, ] <- lambdas[1:(qq - 1), ]
+
+
+      SMA1 <- 0
+      SMA2 <- 0
+      for (kk in 1:qq) {
+        rres1 <- t(as.matrix(lambdaant[kk, ])) %*% solve((sigmal * (diag(1, 2) %x% Delta))) %*% as.matrix(lambdas[kk, ])
+        SMA1 <- SMA1 + rres1
+        rres2 <- t(as.matrix(lambdaant[kk, ])) %*% solve((sigmal * (diag(1, 2) %x% Delta))) %*% as.matrix(lambdaant[kk, ])
+        SMA2 <- SMA2 + rres2
+      }
+
+      VVarvarphi <- (SMA2 + mvarphi)^{
+        -1
+      }
+      MMeanvarphi <- SMA1 * VVarvarphi
+      varphi <- rnorm(1, MMeanvarphi, sqrt(VVarvarphi))
+
+
+
+    }else{
+
+      if(j==(burnin+1)){
+        rm(MDefT,MkappaT,MPhiT)
+      }else{
+
+      }
+
+      if((j%%jump)==0){
+        #source("C:\\Run ApplyDinac\\mainsubrecord.txt")
+
+      }else{
+        #source("C:\\Run ApplyDinac\\mainsub.txt")
+
+      }
+
+
+    }
+
+    print(j)
+  }
+
+}
+
+
---
+++ R/functions_aux.R
@@ -0,0 +1,340 @@
+sintonizarN <- function(taxa, tau, mat, i) {
+  mat <- as.matrix(mat)
+
+
+  mater <- (1 / 50) * sum(mat[(i - 49):i, 1])
+
+  if (mater >= taxa) {
+    delta <- min(0.01, (i / 50 + 1)^(-0.5))
+    temp4 <- log(tau) + delta
+    temp5 <- exp(temp4)
+    return(temp5)
+  } else {
+    delta <- min(0.01, (i / 50 + 1)^(-0.5))
+    temp4 <- log(tau) - delta
+    temp5 <- exp(temp4)
+    return(temp5)
+  }
+}
+
+#
+
+sintonizar <- function(taxa, tau, mat, i) {
+  mat <- as.matrix(mat)
+
+
+
+  mater <- (1 / 50) * sum(mat[(i - 49):i, 1])
+
+  if (mater >= taxa) {
+    delta <- min(0.01, (i / 50 + 1)^(-0.5))
+    temp4 <- log(tau) - delta
+    temp5 <- exp(temp4)
+    return(temp5)
+  } else {
+    delta <- min(0.01, (i / 50 + 1)^(-0.5))
+    temp4 <- log(tau) + delta
+    temp5 <- exp(temp4)
+    return(temp5)
+  }
+}
+#
+FFBSmod <- function(MFFt, GGt, MVVt, WWt, mm0, CC0, yy, ttheta, indices) {
+  tempA <- matrix(NA, nrow(ttheta), ncol(ttheta))
+  Tt <- nrow(yy)
+  mm <- matrix(0, nrow(yy), nrow(mm0))
+  CC <- matrix(0, nrow(yy), (nrow(CC0) * ncol(CC0)))
+
+  mmant <- mm0
+  CCant <- CC0
+
+  for (i in 1:nrow(yy)) {
+    aat <- GGt %*% mmant
+    RRt <- GGt %*% CCant %*% t(GGt) + WWt
+    FFt <- matrix(MFFt[i, ], ncol(ttheta), ncol(yy))
+    fft <- t(FFt) %*% aat
+    VVt <- matrix(MVVt[indices[i], ], ncol(yy), ncol(yy))
+    QQt <- t(FFt) %*% RRt %*% FFt + VVt
+    AAt <- RRt %*% FFt %*% solve(QQt)
+    eet <- as.matrix(yy[i, ]) - fft
+    mmt <- aat + AAt %*% eet
+    mm[i, ] <- mmt
+    CCt <- RRt - AAt %*% QQt %*% t(AAt)
+    CC[i, ] <- as.vector(CCt)
+    mmant <- mmt
+    CCant <- CCt
+  }
+
+  TTt <- nrow(yy)
+  mmm <- as.matrix(mm[TTt, ])
+  CCC <- matrix(CC[TTt, ], ncol(mm), ncol(mm))
+  tempA[TTt, ] <- as.matrix(mvrnorm(1, mmm, CCC))
+
+  for (j in (TTt - 1):1) {
+    mmm <- as.matrix(mm[j, ])
+    CCC <- matrix(CC[j, ], ncol(mm), ncol(mm))
+    varA <- solve(t(GGt) %*% solve(WWt) %*% GGt + solve(CCC))
+    mediaA <- varA %*% (t(GGt) %*% solve(WWt) %*% as.matrix(tempA[(j + 1), ]) + solve(CCC) %*% mmm)
+    tempA[j, ] <- as.matrix(mvrnorm(1, mediaA, varA))
+  }
+  tempA
+}
+#
+FFBSdef <- function(FFt, GGt, VVt, WWt, mm0, CC0, yy, ttheta) {
+  tempA <- matrix(NA, nrow(ttheta), ncol(ttheta))
+  Tt <- nrow(yy)
+  mm <- matrix(0, nrow(yy), nrow(mm0))
+  CC <- matrix(0, nrow(yy), (nrow(CC0) * ncol(CC0)))
+
+  mmant <- mm0
+  CCant <- CC0
+
+  for (i in 1:nrow(yy)) {
+    aat <- GGt %*% mmant
+    RRt <- GGt %*% CCant %*% t(GGt) + WWt
+    fft <- t(FFt) %*% aat
+    QQt <- t(FFt) %*% RRt %*% FFt + VVt
+    AAt <- RRt %*% FFt %*% solve(QQt)
+    eet <- as.matrix(yy[i, ]) - fft
+    mmt <- aat + AAt %*% eet
+    mm[i, ] <- mmt
+    CCt <- RRt - AAt %*% QQt %*% t(AAt)
+    CC[i, ] <- as.vector(CCt)
+    mmant <- mmt
+    CCant <- CCt
+  }
+
+  TTt <- nrow(yy)
+  mmm <- as.matrix(mm[TTt, ])
+  CCC <- matrix(CC[TTt, ], ncol(mm), ncol(mm))
+  tempA[TTt, ] <- as.matrix(mvrnorm(1, mmm, CCC))
+
+  for (j in (TTt - 1):1) {
+    mmm <- as.matrix(mm[j, ])
+    CCC <- matrix(CC[j, ], ncol(mm), ncol(mm))
+    varA <- solve(t(GGt) %*% solve(WWt) %*% GGt + solve(CCC))
+    mediaA <- varA %*% (t(GGt) %*% solve(WWt) %*% as.matrix(tempA[(j + 1), ]) + solve(CCC) %*% mmm)
+    tempA[j, ] <- as.matrix(mvrnorm(1, mediaA, varA))
+  }
+
+  tempA
+}
+#
+SSUMPsi <- function(MAT, THETA, THETAant) {
+  suma <- matrix(0, ncol(THETA), ncol(THETA))
+
+  for (j in 1:nrow(THETA)) {
+    res <- (as.matrix(THETA[j, ]) - MAT %*% as.matrix(THETAant[j, ])) %*% t((as.matrix(THETA[j, ]) - MAT %*% as.matrix(THETAant[j, ])))
+    suma <- suma + res
+  }
+
+  suma
+}
+#
+SSUMd <- function(MAT, THETA, THETAant, DDD) {
+  suma <- 0
+
+  for (j in 1:nrow(THETA)) {
+    res <- t(as.matrix(THETA[j, ]) - MAT %*% as.matrix(THETAant[j, ])) %*% solve(DDD) %*% (as.matrix(THETA[j, ]) - MAT %*% as.matrix(THETAant[j, ]))
+    suma <- suma + res
+  }
+
+  suma
+}
+#
+SSUMmed <- function(MAT, YY, THETA, DDD) {
+  suma <- 0
+
+  for (k in 1:nrow(YY)) {
+    res <- t((as.matrix(YY[k, ]) - t(matrix(MAT[k, ], ncol(THETA), ncol(YY))) %*% as.matrix(THETA[k, ]))) %*% solve(DDD) %*% (as.matrix(YY[k, ]) - t(matrix(MAT[k, ], ncol(THETA), ncol(YY))) %*% as.matrix(THETA[k, ]))
+    suma <- suma + res
+  }
+
+  suma
+}
+#
+confh <- function(vetor) {
+  n <- length(vetor)
+
+  w <- as.matrix(H(n) %*% vetor)
+  escala <- as.numeric(sqrt(t(Conj(w)) %*% w))
+  z <- (1 / escala) * w
+  res <- list(z, escala)
+}
+#
+H <- function(k) {
+  matriz <- array(0, dim = c((k - 1), k))
+
+  for (i in 1:(k - 1)) {
+    for (j in 1:k) {
+      if (j <= i) {
+        matriz[i, j] <- -(i * (i + 1))^(-1 / 2)
+      } else {
+        if (j == (i + 1)) {
+          matriz[i, j] <- -i * (-(i * (i + 1))^(-1 / 2))
+        } else {
+
+        }
+      }
+    }
+  }
+
+  matriz
+}
+#
+gCorr <- function(bb, dd) {
+  n <- nrow(dd)
+  R <- exp(-bb * (as.matrix(dist(dd))))
+  R
+}
+#
+amostrard <- function(DDef, Sr, Rde, MFFT, yTT, TTheta, indGama, kkappa, sssigma, PPhi, llambda, ssigmad, DDelta, u1) {
+  QQ <- ssigmad * diag(1, 2) %x% DDelta
+  Z0 <- as.matrix(as.vector(Sr))
+  n <- nrow(DDef) / 2
+  Do <- complex(real = DDef[1:(n), 1], imaginary = DDef[(n + 1):(2 * n), 1]) # Coordenadas complexas
+  ztemp <- confh(Do) ## Removendo translação e escala
+  z <- ztemp[[1]] ## Pre-forma de D actual
+  esc <- ztemp[[2]] ## valor da escala removida
+  marcoc1 <- complex(real = Sr[, 1], imaginary = Sr[, 2])
+  Zotemp <- confh(marcoc1)
+  Zo <- Zotemp[[1]] # Pre-forma de referencia
+  theta <- Arg(t(Conj(z)) %*% Zo)
+  ww <- as.vector(exp(theta * (0 + 1i))) * z
+  zd <- t(H(n)) %*% ww ## Configuração Pre-forma de D actual
+  vectD <- c(Re(zd), Im(zd)) # Configuração pre-forma de D actual vetorizada
+  ssigma <- u1 * diag(rep(1, 2)) %x% Rde # Matriz de covariancia proposta função de transição
+  vectdprop <- mvrnorm(1, vectD, ssigma) # Geração de configuração Pre-forma proposta vetorizada
+  Dp <- cbind(as.matrix(vectdprop[1:n]), as.matrix(vectdprop[(n + 1):(2 * n)])) # Configuração proposta pre-forma
+  dprop <- esc * Dp # D proposto escala original
+  vecdprop <- as.matrix(c(dprop[, 1], dprop[, 2]))
+
+  INN <- diag(1, ncol(yTT))
+
+  ddeff <- cbind(as.matrix(DDef[1:(nrow(DDef) / 2), 1]), as.matrix(DDef[(nrow(DDef) / 2 + 1):nrow(DDef), 1]))
+  SSS <- sssigma * ((1 - kkappa) * gCorr(PPhi, ddeff) + kkappa * INN)
+
+  SUM <- 0
+  for (j in indGama) {
+    FFT <- matrix(MFFT[j, ], ncol(TTheta), ncol(yTT))
+    SUM <- SUM + t(as.matrix(yTT[j, ]) - t(FFT) %*% as.matrix(TTheta[j, ])) %*% solve(SSS) %*% (as.matrix(yTT[j, ]) - t(FFT) %*% as.matrix(TTheta[j, ]))
+  }
+
+  logp <- -(length(indGama) / 2) * log(det(SSS)) - 0.5 * SUM - 0.5 * t(DDef - Z0 - llambda) %*% solve(QQ) %*% (DDef - Z0 - llambda)
+
+  ddeffprop <- cbind(as.matrix(vecdprop[1:(nrow(vecdprop) / 2), 1]), as.matrix(vecdprop[(nrow(vecdprop) / 2 + 1):nrow(vecdprop), 1]))
+  SSSp <- sssigma * ((1 - kkappa) * gCorr(PPhi, ddeffprop) + kkappa * INN)
+
+  SUMp <- 0
+  for (j in indGama) {
+    FFT <- matrix(MFFT[j, ], ncol(TTheta), ncol(yTT))
+    SUMp <- SUMp + t(as.matrix(yTT[j, ]) - t(FFT) %*% as.matrix(TTheta[j, ])) %*% solve(SSSp) %*% (as.matrix(yTT[j, ]) - t(FFT) %*% as.matrix(TTheta[j, ]))
+  }
+
+  logpprop <- -(length(indGama) / 2) * log(det(SSSp)) - 0.5 * SUMp - 0.5 * t(vecdprop - Z0 - llambda) %*% solve(QQ) %*% (vecdprop - Z0 - llambda)
+
+  prob <- min(exp((logpprop) - (logp)), 1)
+
+  u <- runif(1, 0, 1)
+
+  if (u < prob) {
+    dprox <- vecdprop
+
+    rejei <- 1
+  } else {
+    dprox <- DDef
+    rejei <- 0
+  }
+
+
+
+
+
+
+  res <- list(dprox, rejei)
+  res
+}
+#
+amostrarkapa <- function(MFFT, yTT, TTheta, kkappa, ssigma, PPhi, MDEF, aa1z, aa1e, bb1z, bb1e, u1, u2) {
+  INN <- diag(1, ncol(yTT))
+  ddeff <- matrix(MDEF, ncol(yTT), 2)
+  SSS <- ssigma * ((1 - kkappa) * gCorr(PPhi, ddeff) + kkappa * INN)
+  SUM <- SSUMmed(MFFT, yTT, TTheta, SSS)
+
+  logp <- (aa1z - 1) * log(kkappa) + (aa1e - 1) * log(1 - kkappa) - (aa1e + aa1z) * log(bb1z * kkappa + bb1e * (1 - kkappa)) - (nrow(MFFT) / 2) * log(det(SSS)) - 0.5 * SUM
+
+  kkappaprop <- rbeta(1, kkappa * (u1 + u2), (u1 + u2) * (1 - kkappa))
+
+  if ((kkappaprop <= 0.0001) | (kkappaprop >= 0.9999)) {
+    return(list(kkappa, 0))
+  } else {
+
+  }
+
+  SSSp <- ssigma * ((1 - kkappaprop) * gCorr(PPhi, ddeff) + kkappaprop * INN)
+  SUMp <- SSUMmed(MFFT, yTT, TTheta, SSSp)
+
+  logpprop <- (aa1z - 1) * log(kkappaprop) + (aa1e - 1) * log(1 - kkappaprop) - (aa1e + aa1z) * log(bb1z * kkappaprop + bb1e * (1 - kkappaprop)) - (nrow(MFFT) / 2) * log(det(SSSp)) - 0.5 * SUMp
+
+  logprob <- logpprop + log(dbeta(kkappa, kkappaprop * (u1 + u2), (u1 + u2) * (1 - kkappaprop))) - (logp + log(dbeta(kkappaprop, kkappa * (u1 + u2), (u1 + u2) * (1 - kkappa))))
+
+  prob <- min(c(1, exp(logprob)))
+
+  u <- runif(1, 0, 1)
+
+
+  prob <- min(c(1, exp(logprob)))
+
+  u <- runif(1, 0, 1)
+
+  if (u < prob) {
+    kapaprox <- kkappaprop
+
+    rejei <- 1
+  } else {
+    kapaprox <- kkappa
+
+    rejei <- 0
+  }
+
+
+
+  res <- list(kapaprox, rejei)
+  res
+}
+#
+amostrarphi <- function(MFFT, yTT, TTheta, kkappa, ssigma, PPhi, MDEF, aa1, bb1, u1) {
+  INN <- diag(1, ncol(yTT))
+  ddeff <- matrix(MDEF, ncol(yTT), 2)
+  SSS <- ssigma * ((1 - kkappa) * gCorr(PPhi, ddeff) + kkappa * INN)
+  SUM <- SSUMmed(MFFT, yTT, TTheta, SSS)
+
+  logp <- -(nrow(MFFT) / 2) * log(det(SSS)) - 0.5 * SUM - bb1 / PPhi - (aa1 + 1) * log(PPhi)
+
+  PPhiprop <- rgamma(1, shape = PPhi * u1, rate = u1)
+  SSSp <- ssigma * ((1 - kkappa) * gCorr(PPhiprop, ddeff) + kkappa * INN)
+  SUMp <- SSUMmed(MFFT, yTT, TTheta, SSSp)
+
+  logpprop <- -(nrow(MFFT) / 2) * log(det(SSSp)) - 0.5 * SUMp - bb1 / PPhiprop - (aa1 + 1) * log(PPhiprop)
+
+  logprob <- logpprop + log(dgamma(PPhi, shape = PPhiprop * u1, rate = u1)) - (logp + log(dgamma(PPhiprop, shape = PPhi * u1, rate = u1)))
+  prob <- min(c(1, exp(logprob)))
+
+
+  u <- runif(1, 0, 1)
+
+  if (u < prob) {
+    bprox <- PPhiprop
+
+    rejei <- 1
+  } else {
+    bprox <- PPhi
+
+    rejei <- 0
+  }
+
+
+
+  res <- list(bprox, rejei)
+  res
+}
