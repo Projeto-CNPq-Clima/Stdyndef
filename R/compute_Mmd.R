af06e4e4c0ea6cd67a876a529abd93778f9e34dc
Carla <carlaapolinario960@gmail.coml>
2024-11-11 18:43:58 -0300

    mmd compute

--- R/compute_Mmd.R
+++ R/compute_Mmd.R
@@ -4,8 +4,33 @@
 #' @param response A matrix of observed values for the response variable.
 #' @param sites A matrix containing the geographic coordinates of the locations.
 #' @param Map A matrix representing the geographic coordinates of the map boundaries.
-#' @param dd The number of divisions for the grid.
-#' @return Mmd A matrix with computed deformations based on the MCMC output.
+#' @param dd An integer specifying the resolution of the grid, where the estimated deformations will be
+#' computed for a grid of dimension `(dd+1) x (dd+1)`.
+#'
+#' @return A matrix where each column represents the posterior estimate of the q-th dynamic deformation
+#' on a grid of dimension `(dd+1) x (dd+1)`. Additionally, it includes the `Map` outline as a reference
+#' for spatial context.
+#'
+#' @details
+#' This function calculates the dynamic deformations by using posterior samples obtained from MCMC estimation.
+#' For each posterior sample, the deformations are interpolated over a specified grid resolution `(dd+1) x (dd+1)`,
+#' with each column in the resulting matrix corresponding to the posterior deformation estimates at that grid
+#' resolution. The `Map` outline is returned alongside these estimates for geographic reference.
+#'
+#' @examples
+#' # Example usage of compute_Mmd
+#' MCMC_output <- SpatialDeformationMCMC(response = temperature, FT = FT, MatFFT = MatFFT, GT = GT,
+#'                                        sites = sites, GAMA = GAMA, iteration = 100, burnin = 50, jump = 1)
+#' response <- temperature
+#' sites <- sites
+#' Map <- Map
+#' dd <- 10
+#'
+#' Mmd_result <- compute_Mmd(MCMC_output, response, sites, Map, dd)
+#'
+#' # Plot the posterior deformations on the grid with Map outline
+#' image(matrix(Mmd_result[,1], nrow = dd + 1), main = "Posterior Deformation (q = 1)")
+#' lines(Map[,1], Map[,2], col = "red")
 #'
 #' @export
 compute_Mmd <- function(MCMC_output, response, sites, Map, dd) {
